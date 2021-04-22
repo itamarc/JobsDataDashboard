f1f14374d782842b0dd075ed5fb7d409f1b32353
Itamar <itamarc@gmail.com>
2021-04-22 00:41:39 -0300

    First source code and some docs

---
+++ src/GrabberJob.py
@@ -0,0 +1,18 @@
+'''
+GrabberJob.py
+by Itamar Carvalho
+This file is part of the Job Seeker Project stored in:
+https://github.com/itamarc/JobSeekerProject/
+
+- This job will run periodically (initially can be once a day).
+- This job will connect to public job listings APIs (using REST or other method)
+and save the data loaded in an "inbox" database for further processing.
+- After saving the data, this job will trigger the JobsAnalyzer.
+'''
+
+# Load configuration
+    # Load job listings APIs
+# For each API
+    # Grab data
+    # Save data
+
---
+++ src/JSPConfigFileCreator.py
@@ -0,0 +1,15 @@
+import yaml
+
+config_file = r'D:\itamar\Documents\Pessoal\Estudos\JobSeekerProject\src\config-sample.yaml'
+
+config = [ {'job_sites': [{'name': 'USAJOBS',
+                           'url': 'https://data.usajobs.gov/api/search',
+                           'host': 'data.usajobs.gov',
+                           'method': 'GET',
+                           'User-Agent': 'your@email.address',
+                           'Authorization-Key': 'YourAPIKey' }]},
+           {'job_queries' : [{'job_name': 'Software Engineer'},
+                             {'job_name': 'Backend Developer'}]} ]
+
+with open(config_file, 'w') as file:
+    documents = yaml.dump(config, file)
