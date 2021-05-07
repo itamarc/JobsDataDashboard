c5ee9162b08e49a4acf935bea99dc8657a40efbd
Itamar Carvalho <itamarc@gmail.com>
2021-05-07 19:23:21 -0300

    Python code moved to another repository
    This repository will have only the project documentation from now on.

--- src/jddgrabber/MuseDataGrabber.py
+++
@@ -1,8 +0,0 @@
-from jddgrabber.DataGrabber import DataGrabber
-
-
-class MuseDataGrabber(DataGrabber):
-    def fetch_data(self):
-        print("This method will fetch the data from the Muse REST service.")
-        print("params: ", self.params)
-        return {}
